ef0fe479beda56d7c37a0d6d3224af84222c5396
Akash1072004 <akashkumar1072004@gmail.com>
2026-03-04 11:45:25 +0530

    Longest Bitnoic Subsequence

---
+++ StriverSheet practice/Longest_Bitnoic_Subsequence.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int LongestBitonicSequence(vector<int> arr) {
+        int n = arr.size();
+
+        vector<int> dp1(n, 1);
+
+        // for LIS from Left to Right
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(arr[i] > arr[j] && dp1[j]+1 > dp1[i]){
+                    dp1[i] = dp1[j] + 1;
+                }
+            }
+        }
+
+        // For LIS form Right to Left
+
+        vector<int> dp2(n, 1);
+
+        for(int i = n-1; i >= 0; i--){
+            for(int j = n-1; j > i; j--){
+                if(arr[i] > arr[j] && dp2[j]+1 > dp2[i]){
+                    dp2[i] = dp2[j] + 1;
+                }
+            }
+        }
+
+        // Find the Maximum Lenght of Bitnoic Subsequence
+
+        int maxi = 0;
+
+        for(int i = 0; i < n; i++){
+            maxi = max(maxi, dp1[i] + dp2[i] - 1);
+        }
+
+        return maxi;
+
+
+
+    }
+};
+int main(){
+
+}
